f44f5b5139ab2f1daa17c3acf11684d7fb95d3da
Jamie Gammel <jmgsquared214@gmail.com>
2024-07-05 13:16:00 -0600

    addig desktop hero image

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/font';
 import AcmeLogo from './ui/acme-logo';
+import Image from 'next/image';
+
 
 export default function Page() {
   return (
@@ -11,18 +13,28 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         <AcmeLogo />
       </div>
+      <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
+        {/* Add Hero Images Here */}
+        <Image
+          src="/hero-desktop.png"
+          width={1000}
+          height={760}
+          className="hidden md:block"
+          alt="Screenshots of the dashboard project showing desktop version"
+        />
+      </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            
-            <strong>Welcome to Riverlyn LLC.</strong> Assets, Unclaimed Property, Audits, Compliance and Solutions{' '}
+          className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+         <div>
+         <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
+          <strong>Welcome to Riverlyn LLC.</strong> Assets, Unclaimed Property, Audits, Compliance and Solutions{' '}
             <div><a href="https://riverlynllc.com" className="text-blue-500">
               Search For Your Assets!
             </a></div>
           </p>
-          <p
-            className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
-          ></p>
+         </div>
+        
           <Link
             href="/login"
             className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base">
@@ -33,7 +45,6 @@ export default function Page() {
 
         </div>
         Developed and Hosted by Jamie Riverlyn.
-
       </div>
     </main>
   );
